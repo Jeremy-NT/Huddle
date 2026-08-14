8e8ac5efccbb1ecee17492b288a4a0e7be899e52
Jeremiah <pokentifafa@gmail.com>
2026-08-14 13:30:50 +0000

    add docker file and dockerignore implement express server with static file rendering

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,10 +4,13 @@ import 'dotenv/config.js'; // Load environment variables from .env file
 import User from '../models/user.model.js';
 import {clerkMiddleware} from '@clerk/express';
 import cors from 'cors';
+import fs from fs; // Import the file system module to handle file operations
+import path from 'path'; // Import the path module to handle file and directory paths
 
 const PORT = process.env.PORT || 3000; // Use the PORT from environment variables or default to 3000
 const app = express(); // Create an instance of the Express application
 const FRONTEND_URL = process.env.FRONTEND_URL
+const publicDir = path.join(process.cwd(), 'public'); // Define the path to the public directory for serving static files
 
 app.use(express.json()); // Middleware to parse incoming JSON requests
 app.use(cors({origin:FRONTEND_URL, credentials: true})); // Use CORS middleware to handle cross-origin resource sharing
@@ -17,6 +20,13 @@ app.get('/health', (req, res) => {
   res.status(200).send('Server is running!');
 }); // Define a health check endpoint to verify that the server is running
 
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir)); // Serve static files from the public directory if it exists
+
+  app.get("/{any*}", (req, res, next) => {
+  res.sendFile(path.join(publicDir, 'index.html'), (err) => next (err))
+ })/// Serve the index.html file for any unmatched routes
+}
 app.listen(PORT, () => {
     connectDB();
     console.log(`Server is running on port ${PORT}`);
